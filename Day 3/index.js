d7e7efb00e00de808a9c8630c602151be2f28997
xWafl <73sampleperson@gmail.com>
2019-12-03 14:17:15 +0000

    got myself ready for day 4

--- Day 3/index.js
+++ Day 3/index.js
@@ -11,136 +11,124 @@ fs.readFile('Day 3/solution.txt', 'utf-8', (err, data) => {
         splitData[i] = splitData[i].split(",")
     }
     // console.log(splitData);
-    let board = [];
-    for (let i = 0; i < 10000; i++) {
-        board.push([]);
-        for (let j = 0; j < 10000; j++) {
-            board[i].push(0);
+    let board = {};
+    const location = (loc) => (((loc[0] + loc[1]) * (loc[0] + loc[1] + 1) * 0.5) + loc[1])
+    // const checkInter = (loc) => {
+    //     if (board[location([loc[0],loc[1]])] >= 2 && loc[0] !== center[0] && loc[1] !== center[1]) {
+    //         crossed.push(location(loc));
+    //         intersections.push([loc[1], loc[0]]);
+    //         mandist.push((Math.abs(loc[1] - center[1]) + Math.abs(loc[0] - center[0])));
+    //         return 1;
+    //     }
+    //     return 0;
+    // }
+    const arrIn = (val, arr) => {
+        for (let value of arr) {
+            if (val == value) {
+                return false;
+            }
         }
-    }
-    const checkInter = (loc) => {
-        if (board[loc[0]][loc[1]] === 2 && loc[0] !== center[0] && loc[1] !== center[1]) {
-            intersections.push([loc[1], loc[0]]);
-            mandist.push((Math.abs(loc[1] - center[1]) + Math.abs(loc[0] - center[0])));
-            return 1;
-        }
-        return 0;
+        return true;
     }
     const center = [5000, 5000];
-    board[5000][5000] = 1;
+    board[location([5000, 5000])] = 1;
+    let realboard = [{}, {}];
+    let inters = [[],[]];
     let currentMove = [5000, 5000];
-    let intersections = [];
-    let mandist = [];
-    for (let wire of splitData) {
-    currentMove = [5000, 5000];
-    console.log("new wire");
-    for (let command of wire) {
-        // console.log(command.slice(1));
-        switch (command.charAt(0)) {
-            case "U":
-                for (let i = 0; i < Number(command.slice(1)); i++) {
-                    currentMove[0]++;
-                    // console.log(board[currentMove[0]][currentMove[1]])
-                    board[currentMove[0]][currentMove[1]]++;
-                }
-                break;
-            case "R":
-                for (let i = 0; i < Number(command.slice(1)); i++) {
-                    currentMove[1]++;
-                    board[currentMove[0]][currentMove[1]]++;
-                }
-                break;
-            case "L":
-                for (let i = 0; i < Number(command.slice(1)); i++) {
-                    currentMove[1]--;
-                    board[currentMove[0]][currentMove[1]]++;
-                }
-                break;
-            case "D":
-                for (let i = 0; i < Number(command.slice(1)); i++) {
-                    currentMove[0]--;
-                    board[currentMove[0]][currentMove[1]]++;
-                }
-                break;
+    for (let j = 0; j < 2; j++) {
+        currentMove = [5000, 5000];
+        let crossed = [];
+        let step = 0;
+        var intersections = inters[j];
+        for (let command of splitData[j]) {
+            // console.log(command.slice(1));
+            switch (command.charAt(0)) {
+                case "U":
+                    for (let i = 0; i < Number(command.slice(1)); i++) {
+                        step++;
+                        currentMove[0]++;
+                        // console.log(board[currentMove[0]][currentMove[1]])
+                        if (arrIn(location(currentMove), crossed)) {
+                            if (board[location(currentMove)]) {
+                                crossed.push(location(currentMove));
+                                intersections.push([...currentMove]);
+                                board[location(currentMove)]++;
+                                realboard[j][location(currentMove)] = step;
+                            } else {
+                                board[location(currentMove)] = 1;
+                                realboard[j][location(currentMove)] = step;
+                            }
+                        }
+                    }
+                    break;
+                case "R":
+                    for (let i = 0; i < Number(command.slice(1)); i++) {
+                        step++;
+                        currentMove[1]++;
+                        if (arrIn(location(currentMove), crossed)) {
+                            if (board[location(currentMove)]) {
+                                crossed.push(location(currentMove));
+                                intersections.push([...currentMove]);
+                                board[location(currentMove)]++;
+                                realboard[j][location(currentMove)] = step;
+                            } else {
+                                board[location(currentMove)] = 1;
+                                realboard[j][location(currentMove)] = step;
+                            }
+                        }
+                    }
+                    break;
+                case "L":
+                    for (let i = 0; i < Number(command.slice(1)); i++) {
+                        step++;
+                        currentMove[1]--;
+                        if (arrIn(location(currentMove), crossed)) {
+                            if (board[location(currentMove)]) {
+                                crossed.push(location(currentMove));
+                                intersections.push([...currentMove]);
+                                board[location(currentMove)]++;
+                                realboard[j][location(currentMove)] = step;
+                            } else {
+                                board[location(currentMove)] = 1;
+                                realboard[j][location(currentMove)] = step;
+                            }
+                        }
+                    }
+                    break;
+                case "D":
+                    for (let i = 0; i < Number(command.slice(1)); i++) {
+                        step++;
+                        currentMove[0]--;
+                        if (arrIn(location(currentMove), crossed)) {
+                            if (board[location(currentMove)]) {
+                                crossed.push(location(currentMove));
+                                intersections.push([...currentMove]);
+                                board[location(currentMove)]++;
+                                realboard[j][location(currentMove)] = step;
+                            } else {
+                                board[location(currentMove)] = 1;
+                                realboard[j][location(currentMove)] = step;
+                            }
+                        }
+                    }
+                    break;
+            }
+            // console.log(crossed);
+            // console.log(intersections.length);
+            // console.log(board);
+            // console.log("Current amt: " + board[currentMove[0]][currentMove[1]]);
         }
-        // console.log(board);
-        // console.log("Current amt: " + board[currentMove[0]][currentMove[1]]);
     }
-    }
-    for (let i = 0; i < 10000; i++) {
-        for (let j = 0; j < 10000; j++) {
-            checkInter([i, j]);
+    // for (let i in board) {
+    //     checkInter(i);
+    // }
+    let mandist = 9999999;
+    for (let val of intersections) {
+        if (mandist > (realboard[0][location([val[0], val[1]])] + realboard[1][location([val[0], val[1]])])) {
+            mandist = (realboard[0][location([val[0], val[1]])] + realboard[1][location([val[0], val[1]])])
+            console.log(realboard[0][location([val[0], val[1]])] + realboard[1][location([val[0], val[1]])]);
         }
     }
-    console.log(intersections);
-    console.log(Array.min(mandist));
+    // console.log(intersections);
+    console.log("Value: " + mandist);
 })
-
-/*
-
-const fs = require('fs');
-
-const inputs = fs.readFileSync('solution.txt',{encoding:'utf8'}).toString().split('\n')
-    const wire1 = inputs[0].split(',')
-    const wire2 = inputs[1].split(',')
-    let map = {}, lowest = 999999
-    function runWire(wire, check = false) {
-        var x = 0, y = 0
-        for (var i = 0; i < wire.length; i++) {
-            var op = wire[i][0]
-            var ct = parseInt(wire[i].substring(1))
-            var xx = 0, yy = 0
-            if (op == "R") xx = 1
-            if (op == "L") xx = -1
-            if (op == "U") yy = 1
-            if (op == "D") yy = -1
-            for (var b = 0; b < ct; b++) {
-                x += xx, y += yy
-                if (!check) {
-                  map[x + '_' + y] = 1
-                } else {
-                  if (map[x + '_' + y] == 1) {
-                    var dist = Math.abs(x) + Math.abs(y)
-                    if (dist < lowest) lowest = dist
-                }
-            }
-        }
-    } 
-}
-  
-runWire(wire1)
-runWire(wire2, true)
-console.log(lowest)
-let map1steps = {}
-map = {}, lowest = 999999
-
-function runWire2(wire, check = false) {
-    var x = 0, y = 0, steps = 0
-    for (var i = 0; i < wire.length; i++) {
-        var op = wire[i][0]
-        var ct = parseInt(wire[i].substring(1))
-        var xx = 0, yy = 0
-        if (op == "R") xx = 1
-        if (op == "L") xx = -1
-        if (op == "U") yy = 1
-        if (op == "D") yy = -1
-        for (var b = 0; b < ct; b++) {
-            steps++, x += xx, y += yy
-            if (!check) {
-                map[x + '_' + y] = 1
-                if (!map1steps[x + '_' + y]) map1steps[x + '_' + y] = steps
-            } else {
-                if (map[x + '_' + y] == 1) {
-                    var dist = map1steps[x + '_' + y] + steps
-                    if (dist < lowest) lowest = dist
-                }
-            }
-        }
-    }  
-}
-
-runWire2(wire1)
-runWire2(wire2, true)
-console.log(lowest)
-
-*/
